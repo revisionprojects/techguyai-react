5922b22b985bc5fbf612986fae321a6d2a02d93c
Userma1 <49586013+Userma1@users.noreply.github.com>
2024-10-05 12:15:29 -0400

    change gallery to be 4 per row

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ const videos = [
   { id: '2', title: 'The Age of AI: Artificial Intelligence, the Future of Humankind', thumbnail: 'https://img.youtube.com/vi/5J5bDQHQR1g/maxresdefault.jpg', videoId: '5J5bDQHQR1g' },
   { id: '3', title: 'Artificial Intelligence: Mankind\'s Last Invention', thumbnail: 'https://img.youtube.com/vi/Pls_q2aQzHg/maxresdefault.jpg', videoId: 'Pls_q2aQzHg' },
   { id: '4', title: 'The Future of Artificial Intelligence', thumbnail: 'https://img.youtube.com/vi/uawLjkSI7Mo/maxresdefault.jpg', videoId: 'uawLjkSI7Mo' },
+  { id: '5', title: 'How AI is Changing the World', thumbnail: 'https://img.youtube.com/vi/O5xeyoRL95U/maxresdefault.jpg', videoId: 'O5xeyoRL95U' },
+  { id: '6', title: 'The Ethics of Artificial Intelligence', thumbnail: 'https://img.youtube.com/vi/3oYBZU5GCzA/maxresdefault.jpg', videoId: '3oYBZU5GCzA' },
+  { id: '7', title: 'AI and the Future of Jobs', thumbnail: 'https://img.youtube.com/vi/v95GjbzB5Zk/maxresdefault.jpg', videoId: 'v95GjbzB5Zk' },
+  { id: '8', title: 'Machine Learning: Living in the Age of AI', thumbnail: 'https://img.youtube.com/vi/ZJixNvx9BAc/maxresdefault.jpg', videoId: 'ZJixNvx9BAc' },
 ];
 
 function App() {
